cee661c59fc5e0e436a0757702a055640e1ec861
Igor Tarasov <tarasov.igor@gmail.com>
2023-05-31 19:15:19 +0300

    Extract compact encrypter, move code there

--- src/CompactEncrypter.php
+++ src/CompactEncrypter.php
@@ -2,19 +2,13 @@
 
 namespace TiGR\CompactEncrypter;
 
-use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
-use Illuminate\Contracts\Encryption\EncryptException;
 use Illuminate\Encryption\Encrypter;
 use RuntimeException;
+use TiGR\CompactEncrypter\Encrypter as CompactEncrypterCore;
 
 final class CompactEncrypter implements EncrypterContract
 {
-    const SUPPORTED_KEY_SIZES = [
-        'AES-128-CBC' => 16,
-        'AES-256-CBC' => 32,
-    ];
-
     /**
      * The encryption key.
      *
@@ -28,6 +22,11 @@ final class CompactEncrypter implements EncrypterContract
      * @var string
      */
     protected $cipher;
+
+    /**
+     * @var CompactEncrypterCore
+     */
+    protected $encrypter;
 
     /** @var Encrypter */
     private $laravelEncrypter;
@@ -43,16 +42,9 @@ final class CompactEncrypter implements EncrypterContract
      */
     public function __construct(string $key, string $cipher = 'AES-128-CBC')
     {
-        if (!static::supported($key, $cipher)) {
-            if (!static::isCipherSupported($cipher)) {
-                throw new RuntimeException('The only supported ciphers are AES-128-CBC and AES-256-CBC.');
-            } else {
-                throw new RuntimeException(sprintf('Invalid key length (%d) for %s cipher.', strlen($key), $cipher));
-            }
-        }
-
-        $this->key = $key;
+        $this->encrypter = new CompactEncrypterCore($key, $cipher);
         $this->cipher = $cipher;
+        $this->key = $key;
     }
 
     /**
@@ -64,12 +56,7 @@ final class CompactEncrypter implements EncrypterContract
      */
     public static function supported(string $key, string $cipher): bool
     {
-        return self::isCipherSupported($cipher) ? self::SUPPORTED_KEY_SIZES[$cipher] == strlen($key) : false;
-    }
-
-    private static function isCipherSupported(string $cipher): bool
-    {
-        return isset(self::SUPPORTED_KEY_SIZES[$cipher]);
+        return CompactEncrypterCore::supported($key, $cipher);
     }
 
     /**
@@ -81,7 +68,7 @@ final class CompactEncrypter implements EncrypterContract
      */
     public static function generateKey(string $cipher): string
     {
-        return random_bytes(self::SUPPORTED_KEY_SIZES[$cipher]);
+        return CompactEncrypterCore::generateKey($cipher);
     }
 
     /**
@@ -96,31 +83,13 @@ final class CompactEncrypter implements EncrypterContract
      */
     public function encrypt($value, $serialize = true, bool $useMac = true): string
     {
-        $iv = random_bytes(openssl_cipher_iv_length($this->cipher));
-
-        // First we will encrypt the value using OpenSSL. After this is encrypted we
-        // will proceed to calculating a MAC for the encrypted value so that this
-        // value can be verified later as not having been changed by the users.
-        $value = openssl_encrypt(
-            $serialize ? serialize($value) : $value,
-            $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv
-        );
-
-        if ($value === false) {
-            throw new EncryptException('Could not encrypt the data.');
+        try {
+            return $this->encrypter->encrypt($value, $serialize, $useMac);
+        } catch (EncryptException $exception) {
+            throw new \Illuminate\Contracts\Encryption\EncryptException(
+                $exception->getMessage(), $exception->getCode(), $exception
+            );
         }
-
-        // Once we get the encrypted value we'll go ahead and base64_encode the input
-        // vector and create the MAC for the encrypted value so we can then verify
-        // its authenticity. Then, we'll JSON the data into the "payload" array.
-        if ($useMac) {
-            $mac = $this->hash($iv, $value);
-            $pack = pack('a20a16a*', $mac, $iv, $value);
-        } else {
-            $pack = pack('a16a*', $iv, $value);
-        }
-
-        return $this->base64_encode($pack);
     }
 
     /**
@@ -152,20 +121,13 @@ final class CompactEncrypter implements EncrypterContract
             return $this->getLaravelEncrypter()->decrypt($payload, $unserialize);
         }
 
-        $payload = $this->getPayload($payload, $useMac);
-
-        // Here we will decrypt the value. If we are able to successfully decrypt it
-        // we will then unserialize it and return it out to the caller. If we are
-        // unable to decrypt this value we will throw out an exception message.
-        $decrypted = openssl_decrypt(
-            $payload['value'], $this->cipher, $this->key, OPENSSL_RAW_DATA, $payload['iv']
-        );
-
-        if ($decrypted === false) {
-            throw new DecryptException('Could not decrypt the data.');
+        try {
+            return $this->encrypter->decrypt($payload, $unserialize, $useMac);
+        } catch (DecryptException $exception) {
+            throw new \Illuminate\Contracts\Encryption\DecryptException(
+                $exception->getMessage(), $exception->getCode(), $exception
+            );
         }
-
-        return $unserialize ? unserialize($decrypted) : $decrypted;
     }
 
     /**
@@ -187,112 +149,13 @@ final class CompactEncrypter implements EncrypterContract
      */
     public function getKey()
     {
-        return $this->key;
-    }
-
-    /**
-     * Create a MAC for the given value.
-     *
-     * @param string $iv
-     * @param mixed $value
-     * @return string
-     */
-    protected function hash($iv, $value)
-    {
-        return hash_hmac('sha1', $iv.$value, $this->key, true);
-    }
-
-    /**
-     * Get the JSON array from the given payload.
-     *
-     * @param string $payload
-     * @param bool $useMac
-     * @return array
-     *
-     */
-    protected function getPayload(string $payload, bool $useMac = true): array
-    {
-        $payload = @unpack(($useMac ? 'a20mac/' : '').'a16iv/a*value', $this->base64_decode($payload));
-
-        // If the payload is not valid JSON or does not have the proper keys set we will
-        // assume it is invalid and bail out of the routine since we will not be able
-        // to decrypt the given value. We'll also check the MAC for this encryption.
-        if (!$this->isValidPayload($payload, $useMac)) {
-            throw new DecryptException('The payload is invalid.');
-        }
-
-        if ($useMac and !$this->isValidMac($payload)) {
-            throw new DecryptException('The MAC is invalid.');
-        }
-
-        return $payload;
-    }
-
-    /**
-     * Verify that the encryption payload is valid.
-     *
-     * @param mixed $payload
-     * @param bool $useMac
-     * @return bool
-     */
-    protected function isValidPayload($payload, bool $useMac = true)
-    {
-        if (!is_array($payload) or !(isset($payload['iv'], $payload['value']))) {
-            return false;
-        }
-
-        if ($useMac and !(isset($payload['mac']) and strlen($payload['mac']) === 20)) {
-            return false;
-        }
-
-        if (strlen($payload['iv']) !== openssl_cipher_iv_length($this->cipher)) {
-            return false;
-        }
-
-        if (strlen($payload['value']) % 16 !== 0) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Determine if the MAC for the given payload is valid.
-     *
-     * @param array $payload
-     * @return bool
-     */
-    protected function isValidMac(array $payload)
-    {
-        return hash_equals($payload['mac'], $this->hash($payload['iv'], $payload['value']));
-    }
-
-    /**
-     * URL-safe base64_encode
-     *
-     * @param string $data
-     * @return string
-     */
-    private function base64_encode(string $data): string
-    {
-        return strtr(rtrim(base64_encode($data), '='), '+/', '-_');
-    }
-
-    /**
-     * URL-safe base64_decode
-     *
-     * @param string $data
-     * @return string
-     */
-    private function base64_decode(string $data): string
-    {
-        return base64_decode(strtr($data, '-_', '+/'));
+        return $this->encrypter->getKey();
     }
 
     private function getLaravelEncrypter(): Encrypter
     {
         if (!isset($this->laravelEncrypter)) {
-            $this->laravelEncrypter = new Encrypter($this->getKey(), $this->cipher);
+            $this->laravelEncrypter = new Encrypter($this->key, $this->cipher);
         }
 
         return $this->laravelEncrypter;
